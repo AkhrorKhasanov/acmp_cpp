9e3c7cea82589f21a563850227d672bbc415fe72
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2026-05-06 09:43:16 +0500

    06.05.2026 09:43

---
+++ Сборник задач В.И. Лукьянчикова/Булева Алгебра/A. Логическое выражение 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    int n, count = 0;
+    cin >> n;
+    vector<bool> v(n + 1, true);
+    v[0] = v[1] = false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (v[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                v[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (v[i])
+        {
+            count++;
+        }
+    }
+    count++;
+    cout << count << endl;
+
+    return 0;
+}
